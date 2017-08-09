34a7c2d91648038f1984798efae80e8fb750890f
Jerd <inquisitivequin@gmail.com>
2017-08-08 18:12:34 -0700

    serv

---
+++ sec-2/basics-assignment-1-start/src/app/warnings/app-warnings.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+@Component ({
+	selector: 'app-warning1',
+	templateUrl: './app-warnings.component.html',
+	styles: [`
+		.warning{ color: red }
+	`]
+})
+
+export class AppWarnings {
+
+}
