d1521aa6638d1fb3e2741ea64f5830d5d9df22d8
Jerd <inquisitivequin@gmail.com>
2017-08-23 19:54:32 -0700

    new comps

--- poat/src/app/app.module.ts
+++ poat/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SuccesscdComponent } from './recipes/recipe-edit/src/app/successcd/successcd.component';
+import { RecipeItemComponent } from './recipes/src/app/recipes/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/src/app/recipes/recipe-detail/recipe-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccesscdComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ poat/src/app/recipes/recipe-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component{(
+	selector: 'recipe-list',
+	templateUrl: './recipe-list.component.html',
+	styleUrls: ['./recipe-list.component.html']
+	)}
+
+export class RecipeList {
+	
+}
